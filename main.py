4272e16aeede2cd706b235ee589ec5e34a3a98c7
Danish Ansari <danishansari9648@gmail.com>
2025-03-10 22:50:44 +0530

    input()

--- main.py
+++ main.py
@@ -84,3 +84,19 @@ name = ""
 name = bool(name)
 print(name)  # False
 
+
+# input() = A function that prompts the user to enter data
+#           Returns the entered data as a string
+
+name = input("What is your name?:")
+age = input("How old are you?:")
+#age = age + 1     #TypeError: can only concatenate str (not "int") to str
+age = int(age)
+# or int(input("How old are you?:"))
+age = age + 1
+
+# or int(input("How old are you?:"))
+print(f"Hello, {name}!")
+print("Happy Birthday!")
+print(f"You are {age} old")
+
