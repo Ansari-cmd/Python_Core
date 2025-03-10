6577a4af9cfcce5b0dcf723913086872f160467f
Danish Ansari <danishansari9648@gmail.com>
2025-03-10 12:39:53 +0530

    FirstCommit

---
+++ main.py
@@ -0,0 +1,35 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# strings
+
+first_name="Danish"
+food="Burger"
+email="danishansari9648@gmail.com"
+print(f"hello {first_name}")
+print(f"you like {food}")
+print(f"your email is {email}")
+
+# Integers
+age=25
+quantity=3
+num_of_studennts=30
+
+print(f"You are {age} years old")
+print(f"you are buying {quantity} items")
+print(f"your class has {num_of_studennts} students")
+
+# Float
+price=10.99
+gpa=3.2
+distance=5.6
+
+print(f"The price is {price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean
+
+is_student=True
+
+print(f"Are you a student?: {is_student}")
