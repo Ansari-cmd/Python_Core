978eef6a1351bfad2218b2da105ac3432a04eda0
Danish Ansari <danishansari9648@gmail.com>
2025-03-10 23:53:40 +0530

    Basic Arithematic Function and Math Function

---
+++ Basic Inbuilt Function/main.py
@@ -0,0 +1,25 @@
+# Basic Arithematic Function
+x = 3.14
+y = 4
+z = 5
+
+result = round(x)  # 3
+print(result)
+result = abs(y)  # -4 to 4
+print(result)
+result = pow(4,3)  # 4x4x4 = 64
+print(result)
+result = max(x,y,z)  # 5
+print(result)
+result = min(x,y,z)  # 3.14
+print(result)
+
+# Math Function
+
+import math
+x = 9
+print(math.pi)
+print(math.e)
+print(math.sqrt(x))
+print(math.ceil(x)) # 9.1 = 10
+print(math.floor(x)) # 9.9 = 9
