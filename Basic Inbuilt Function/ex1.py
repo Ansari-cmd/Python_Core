e545e5fb02740e9931156cce831502ad21dd77a0
Danish Ansari <danishansari9648@gmail.com>
2025-03-11 12:52:32 +0530

    Area of circle

--- Basic Inbuilt Function/ex1.py
+++ Basic Inbuilt Function/ex1.py
@@ -4,4 +4,4 @@ radius = float(input("Enter the radius os a circle: "))
 
 circumference = 2 * math.pi * radius
 
-print(f"The circumference is : {round(circumference,2) }")
+print(f"The circumference is : {round(circumference,2) }cm")
---
+++ Basic Inbuilt Function/ex2.py
@@ -0,0 +1,8 @@
+# Area of circle
+import math
+
+radius = float(input("Enter the radius of circle"))
+
+area = math.pi * pow(radius,2)
+
+print(f"The area of the circle is : {round(area,2)} cm^2")
