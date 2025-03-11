7cb7c86491f7e93f5fb62b29f7581c06e58605e8
Danish Ansari <danishansari9648@gmail.com>
2025-03-11 12:46:09 +0530

    Circumference of circle

---
+++ Basic Inbuilt Function/ex1.py
@@ -0,0 +1,7 @@
+# Circumference of circle
+import math
+radius = float(input("Enter the radius os a circle: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"The circumference is : {round(circumference,2) }")
