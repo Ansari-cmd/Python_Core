e46c1167e444a6236d46cc85cbdc8b63e837d961
Danish Ansari <danishansari9648@gmail.com>
2025-03-12 17:37:05 +0530

    format specifiers

---
+++ Format Specifier/main.py
@@ -0,0 +1,13 @@
+# format specifiers = {value:flags} format a value based on what
+#                              flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center alighn
+# :+ = use a plus sign to indicate positive value
+# := = place sign to left most position
+# :  = insert a space before positive numbers
+# :, = comma seperator
