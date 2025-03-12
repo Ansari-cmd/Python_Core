8438f351a165ba99d45b821ad57024319d861fda
Danish Ansari <danishansari9648@gmail.com>
2025-03-12 17:51:09 +0530

    First 10 Even numbers

---
+++ 100 while loop programs/01.py
@@ -0,0 +1,12 @@
+# First 10 Even numbers
+
+count=1
+num = 0
+while count<=10:
+    if num % 2 == 0:
+        count+=1
+        print(num)
+        #print(f"{num}",end=" ")
+        
+    num+=1
+    
