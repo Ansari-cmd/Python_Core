b6f3d22164f5a8d9d2307af0a1c172f74654b647
Danish Ansari <danishansari9648@gmail.com>
2025-03-12 18:10:56 +0530

    rint first 10 integers and their squares using while loop

---
+++ 100 while loop programs/05.py
@@ -0,0 +1,8 @@
+# Write a program to print first 10 integers and their squares using while loop
+
+count=1
+num = 1
+while count<=10:
+        print(f"Number = {num}, Square of {num} = {num*num} ")
+        count+=1
+        num+=1
